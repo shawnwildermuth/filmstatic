bef2cb8233e39b813d33c2372c563f7b1b695332
Shawn Wildermuth <shawn@wildermuth.com>
2021-10-06 21:25:02 -0400

    Added Supporters

--- utils/filters.js
+++ utils/filters.js
@@ -1,10 +1,12 @@
 const datefns = require("date-fns");
+const { format } = require("date-fns");
+const utcToZonedTime = require("date-fns-tz").utcToZonedTime;
 
 module.exports = {
   format: datefns.format,
   formatISO: datefns.formatISO,
   formatDate: (data, fmt) => {
-    return format(utcToZonedTime(data, "UTC"), "MMMM d, yyyy", { timeZone: "UTC" });
+    return format(utcToZonedTime(data, "UTC"), "MMMM d, yyyy");
   },
   take: (data, num) => {
     if (data instanceof Array) {
@@ -18,6 +20,21 @@ module.exports = {
     if (data instanceof Array) {
       if (!num) num = 25;
       return data.slice(num);
+    }
+
+    return data;
+  },
+  supporterSort: (data) => {
+    if (data instanceof Array) {
+      return data.sort((a, b) => {
+        const items1 = a[0].split(" ");
+        const items2 = b[0].split(" ");
+        const last1 = items1[items1.length - 1];
+        const last2 = items2[items2.length - 1];
+        if (last1 > last2) return 1;
+        if (last1 < last2) return -1;
+        return 0;
+      });
     }
 
     return data;
