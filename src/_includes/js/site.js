36b3dc18f4f1c0c5085c2411a84c49d6ace904cd
shawnwildermuth <shawn@wildermuth.com>
2025-12-25 19:55:14 +0100

    Fix gpdr

--- src/_includes/js/site.js
+++ src/_includes/js/site.js
@@ -1,12 +1,11 @@
 import "./plugins";
 import "./template";
-import gdrp from "./gdpr";
+//import gdrp from "./gdpr";
 import contactform from "./contactform";
 import faconfig from "./faconfig";
 import utils from "./utils";
 
 window.addEventListener("DOMContentLoaded", () => {
-  gdrp();
   contactform();
   faconfig();
   utils();
